cf754da5ee36ac3ac920902e81c2344093ad6224
Lucas Balangero <bobbypoppy1224@gmail.com>
2022-01-06 16:11:41 -0500

    Added function get_valid_moves() to Pacman

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -15,7 +15,27 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		ArrayList<Location> moves = new ArrayList<Location>();
+
+		Location locUp = new Location(myLoc.x, myLoc.y+1);
+		Location locDown = new Location(myLoc.x, myLoc.y-1);
+		Location locRight = new Location(myLoc.x+1, myLoc.y);
+		Location locLeft = new Location(myLoc.x-1, myLoc.y);
+		//Tests if adjacent locations are hitting a wall
+		if (myMap.getLoc(locUp).equals(Map.Type.WALL) || myMap.getLoc(locUp).equals(Map.Type.GHOST)){
+			moves.add(locUp);
+		}
+		if (myMap.getLoc(locDown).equals(Map.Type.WALL) || myMap.getLoc(locDown).equals(Map.Type.GHOST)){
+			moves.add(locDown);
+		}
+		if (myMap.getLoc(locRight).equals(Map.Type.WALL) || myMap.getLoc(locRight).equals(Map.Type.GHOST) ){
+			moves.add(locRight);
+		}
+		if (myMap.getLoc(locLeft).equals(Map.Type.WALL) || myMap.getLoc(locLeft).equals(Map.Type.GHOST)){
+			moves.add(locLeft);
+		}
+		
+		return moves;
 	}
 
 	public boolean move() {
