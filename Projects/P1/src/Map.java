051ae003d12486c2276b88ba842c432cab62c424
Jacob James <jacobredfort@gmail.com>
2022-01-07 17:37:33 -0500

    Map getLoc() and Tests

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -59,8 +59,8 @@ public class Map{
 	}
 	
 	public HashSet<Type> getLoc(Location loc) {
-		//wallSet and emptySet will help you write this method
-		return null;
+		HashSet<Type> alpha = field.get(loc);
+		return alpha;
 	}
 
 	public boolean attack(String Name) {
