c48bf2bcab40bf066ef12d8403bbc53cb23f67c6
Chiebuka Ohams <chiebuka.ohams6@gmail.com>
2022-01-09 15:49:48 -0500

    test for map class attack funtion

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -1,9 +1,12 @@
 import java.awt.Color;
 import junit.framework.*;
 
-public class TestMapAttack {
+public class TestMapAttack extends TestCase{
 
 	public void testMapAttack() {
-		return null;	
+		NoFrame frame = new NoFrame();
+		Ghost ghost = new Ghost(new Location(3, 3), "Danny Phantom", Color.green);
+		Pacman pacman = new Pacman(new Location(3, 2));
+		assertTrue(frame.getMap().attack("Danny Phantom"));
 	}
 }
