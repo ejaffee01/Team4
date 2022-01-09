bdcc8c775c0f00438f6947541f79845f41b117c0
Aiden Stern <aiden.stern@gmail.com>
2022-01-09 14:43:11 -0500

    [add] Map eatCookie test

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -2,9 +2,11 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
-public class TestMapEatCookie {
+public class TestMapEatCookie extends TestCase {
 	
-	public void testMapEatCookie() {
-		return null;
+	public void testMapEatCookie() throws FileNotFoundException {
+		NoFrame frame = new NoFrame();
+        frame.initPlayers();
+        this.assertNotNull(frame.getMap().eatCookie("pacman"));
 	}
 }
