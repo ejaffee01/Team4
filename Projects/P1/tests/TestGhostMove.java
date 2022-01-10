26429b454e18da28d7b5c9cd3dc2db47a9751a64
Lucas Balangero <46410300+lucasoooooo@users.noreply.github.com>
2022-01-10 09:49:32 -0500

    Update TestGhostMove.java

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -6,7 +6,7 @@ public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
-		Ghost ghost  = frame.addGhost(new Location(1, 1));
+		Ghost ghost  = frame.addGhost(new Location(1, 1), "Danny Phantom", Color.green);
 		assertEquals(ghost.move(), true);
 	}
 }
