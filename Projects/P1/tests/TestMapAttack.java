b3adcd887c9d719f47bf42efaa7a5d96eabb2fc2
Lucas Balangero <46410300+lucasoooooo@users.noreply.github.com>
2022-01-10 08:56:35 -0500

    Update TestMapAttack.java

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -1,13 +1,12 @@
 import java.awt.Color;
 import junit.framework.*;
 
-public class TestMapAttack extends TestCase throws FileNotFoundException{
+public class TestMapAttack extends TestCase {
 
-	public void testMapAttack() {
+	public void testMapAttack() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
-		Ghost ghost = frame.addGhost(new Location(3, 3), "Phantom", Color.green);
-		
-		PacMan pacman = frame.addPacMan(new Location(3, 2));
+		frame.addGhost(new Location(3, 3), "Phantom", Color.green);
+		frame.addPacMan(new Location(3, 2));
 
 		assertTrue(frame.getMap().attack("Phantom"));
 	}
