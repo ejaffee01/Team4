aa79ef959312b12c7dd6715766b7183f14a92089
Chiebuka Ohams <chiebuka.ohams6@gmail.com>
2022-01-09 22:22:17 -0500

    added the pacman and ghost to frame

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -5,8 +5,12 @@ public class TestMapAttack extends TestCase throws FileNotFoundException{
 
 	public void testMapAttack() {
 		NoFrame frame = new NoFrame();
-		Ghost ghost = new Ghost(new Location(3, 3), "Danny Phantom", Color.green);
-		Pacman pacman = new Pacman(new Location(3, 2));
-		assertTrue(frame.getMap().attack("Danny Phantom"));
+		Ghost ghost = new Ghost(new Location(3, 3), "Phantom", Color.green);
+		frame.addGhost(new Location(3, 3), "Phantom", Color.green);
+		
+		PacMan pacman = new Pacman(new Location(3, 2));
+		frame.addPacMan(new Location(3, 2));
+
+		assertTrue(frame.getMap().attack("Phantom"));
 	}
 }
