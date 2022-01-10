27a4267aa2c26549d011871ca7e4daec13f3fc8a
Chiebuka Ohams <chiebuka.ohams6@gmail.com>
2022-01-09 22:28:25 -0500

    Collapsed lines to be more concise

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -5,11 +5,9 @@ public class TestMapAttack extends TestCase throws FileNotFoundException{
 
 	public void testMapAttack() {
 		NoFrame frame = new NoFrame();
-		Ghost ghost = new Ghost(new Location(3, 3), "Phantom", Color.green);
-		frame.addGhost(new Location(3, 3), "Phantom", Color.green);
+		Ghost ghost = frame.addGhost(new Location(3, 3), "Phantom", Color.green);
 		
-		PacMan pacman = new Pacman(new Location(3, 2));
-		frame.addPacMan(new Location(3, 2));
+		PacMan pacman = frame.addPacMan(new Location(3, 2));
 
 		assertTrue(frame.getMap().attack("Phantom"));
 	}
